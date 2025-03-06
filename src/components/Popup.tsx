9a14a2438e6fbad8a2b085800d69584def6eebad
magicaiman <aiman.test2003@gmail.com>
2025-03-06 19:31:14 +0100

    use of service for api requests

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,16 +1,14 @@
 import {useEffect, useState} from "react";
+import {getPrayerTimes, prayerTimes} from "../services/PrayerService.ts";
 
 export default function Popup() {
-    const [prayerTimes, setPrayerTimes] = useState<any>(null);
+    const [prayerTimes, setPrayerTimes] = useState<prayerTimes | undefined>();
     useEffect(() => {
-        fetch('https://api.aladhan.com/v1/timingsByCity/05-03-2025?city=Bourg-en-Bresse&country=FR&state=Bourg-en-Bresse&method=3&shafaq=general&tune=5%2C3%2C5%2C7%2C9%2C-1%2C0%2C8%2C-6&timezonestring=Europe%2FParis&calendarMethod=UAQ')
-            .then(res => res.json())
-            .then(data => {
-                setPrayerTimes(data.data.timings)
-            })
-            .catch((error) =>{
-                console.error('Erreur lors de la récupération des heures de priére' ,error)
-            })
+        const fetchTimes = async () => {
+            const times = await getPrayerTimes();
+            setPrayerTimes(times);
+        };
+        fetchTimes();
     }, []);
     return (
         <div>
